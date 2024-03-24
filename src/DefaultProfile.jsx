788f9635c2bbaf71b28708e01c95caaffb310c7f
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-03-24 12:05:14 +0100

    Remove old files, add new files

--- src/DefaultProfile.jsx
+++
@@ -1,5 +0,0 @@
-const DefaultProfile = () => {
-  return <p>Oh, nothing to see here!</p>;
-};
-
-export default DefaultProfile;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './Router';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
